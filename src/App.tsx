d8f705ab57f6761b684c454b8f1db5efc50f187d
mgwaiyanlin <waiyanlinn.netuser@gmail.com>
2024-12-07 15:49:14 +0630

    first-commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// import './App.css'
+import Navbar from "./Navbar"
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <div className="pt-24 container mx-auto">
+        <h3 className="text-2xl font-bold text-center mb-2">Welcome to DevBlog</h3>
+        <p className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam similique quisquam, ex sed nostrum sequi autem officiis accusantium cumque quas.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
